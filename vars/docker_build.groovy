fc1f1ebbdd9708d03158222ca3b2c35f69f54795
Pragya Agrawal <pragya222@gmail.com>
2025-01-28 18:56:18 -0600

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,5 +1,5 @@
 // Define function
 def call(String ProjectName, String ImageTag, String DockerHubUser){
-  sh "sudo usermod -aG docker $USER && newgrp docker"
+  
   sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
